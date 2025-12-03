81c95bc9c1db3da227ce92560992b9b452ee2141
tshepo888 <tshepomoela12@gmail.com>
2025-12-03 17:01:19 +0200

    Remove all root files

--- hours_to_seconds.py
+++
@@ -1,8 +0,0 @@
-# Define the variable
-hours = 2  # Number of hours to convert
-
-# Calculate the number of seconds
-seconds = hours * 3600
-
-# Print the result
-print(f"{hours} hour(s) is {seconds} seconds.")
